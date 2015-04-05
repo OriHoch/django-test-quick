1c12add9a03e1769887cbe25e502893f18d99ac1
Ori Hoch <ori@uumpa.com>
2015-04-05 12:34:44 +0000

    v1.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='django-test-quick',
-    version='1.2.1',
+    version='1.3.0',
     author='Ori Hoch',
     author_email='ori@uumpa.com',
     description='django management command to allow quicker running of tests',
